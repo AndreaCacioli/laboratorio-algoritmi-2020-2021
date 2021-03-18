1e9fa4295505b755e7d7fae1ea463d6fdb8fba43
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2021-03-18 16:45:55 +0100

    Added comments to the header file

--- Exercise1/mbi.h
+++ Exercise1/mbi.h
@@ -1,6 +1,28 @@
 #ifndef SOME_DIFFICULT_NAME_HARD_TO_REMEMBER
 #define SOME_DIFFICULT_NAME_HARD_TO_REMEMBER
 
+
+/**********************************************************************************************
+    *
+    * A library of functions written by Andrea Cacioli to sort arrays of any primitive type
+    * or custom writted records provided that a comparator function pointer is specified for
+    * the algorithm to know the priority of one object compared to the other, similar to how
+    * the C-specific qsort function handles this kind of comparison.
+    * Therefore the size of the array needs to be known at first as well as the size in bytes 
+    * of each individual element.
+    * 
+***********************************************************************************************/
+
+
+/*
+    * The function bi performs a binary-insertion sort:
+    *   Similarly to the classic insertion sort a previously sorted part of the array is given.
+    *   The first element of the array is selected from just out of the sorted part.
+    *   A new position is then found using binary search across the first part of the array
+    * Specifications:
+    *         Binary search is implemented recursively
+    * 
+*/
 extern void* bi(void* v,int (*cmpfnc)(void*, void*), int size, int type_size);
 
 
