3fca31d8d0aacfd18b6cf8ffbea592a7247178ba
andrea.cacioli <andrea.cacioli@edu.unito.it>
2021-03-20 01:02:38 +0100

    Unit tests

--- Exercise1/UnitTests.c
+++ Exercise1/UnitTests.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "mbi.h"
 
+
 int cmp(void* a, void* b)
 {
     return *(int*)a - *(int*)b;
@@ -12,29 +13,36 @@ int revcmp(void* a, void* b)
 {
     return *(int*)b - *(int*)a;
 }
-/*
+
 void test_sort_empty_array() 
 {
     int a[] = {};
     assert(a == bi(a, cmp, 0, sizeof(int)));
+    assert(a == m(a, cmp, 0, sizeof(int)));
 }
 
 void test_sort_null_array() 
 {
     int* a = NULL;
-    assert(a == bi(a, cmp, 0, sizeof(int));
+    assert(a == bi(a, cmp, 0, sizeof(int)));
+    assert(a == m(a, cmp, 0, sizeof(int)));
 }
-*/
+void test_sort_sorted_array() 
+{
+    int a[] = {1,3,5,7,11};
+    int* b = bi(a,cmp, 5, sizeof(int));
+    int* c = m(a,cmp, 5, sizeof(int));
+    for(int i = 0; i < 5; i++)
+    {
+        assert(a[i] == b[i]);
+        assert(a[i] == c[i]);
+    }
+}
+ 
 int main(int argc, char** argv) 
 {
-    //test_sort_empty_array();
-    //test_sort_null_array();
-    int c[] = {1,3,2,4,5,8,3,7,0,14,15,99,43,21,7,6,8,9,42,11,14,67,89,4,1,72,13,69};
-
-    int* t = m(c,revcmp,28,sizeof(int));
-    for(int i = 0; i < 28; i++)
-    {
-        printf("%d\n", t[i]);
-    }
+    test_sort_empty_array();
+    test_sort_null_array();
+    test_sort_sorted_array();
     return 0;
 }
